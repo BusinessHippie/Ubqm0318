cdaf5725feeb5483dc4821d79b0086fb547b3a28
BusinessHippie <38754120+BusinessHippie@users.noreply.github.com>
2019-04-02 11:59:58 +0200

    Add files via upload

--- BikeSharing.R
+++ BikeSharing.R
@@ -16,14 +16,48 @@ library(caret)
 library(tidyverse)
 library(corrplot)
 
-BikeShare <- read.csv(file="C:/Users/alexw/Desktop/Practice_Datasets/Bike_Sharing/hour.csv",header = TRUE)
+BikeShare <- read.csv(file="C:/Users/AlexanderWinkler/Documents/R/hour.csv",header = TRUE)
 
 BikeShare$dteday <- as.POSIXct(BikeShare$dteday)
-BikeShare <- na.omit(BikeShare)
+table(is.na(BikeShare))
+#BikeShare <- na.omit(BikeShare)
+
+str(BikeShare)
+summary(BikeShare)
 
 colnames(BikeShare)[2] <- "Date"
+length(colnames(BikeShare))
 
-BikeShare[,-c(2,3,4,5,6,7)]
+#Check for outliers of bike rental numbers
+boxplot(BikeShare$casual)$out
+boxplot(BikeShare$registered)$out
+boxplot(BikeShare$cnt)$out
+
+Outliers <- boxplot.stats(BikeShare$cnt)
+Outliers_cas <- boxplot.stats(BikeShare$casual)
+Outliers_reg <- boxplot.stats(BikeShare$registered)
+
+summary(BikeShare$cnt)
+
+Outliers$stats
+Outliers$out
+Outliers_reg$out
+#Get respective indices (positions) of the outliers
+a <- which(BikeShare$cnt %in% Outliers$out)
+
+Outliers_cas$out
+#Get respective indices (positions) of the outliers of casual rentals
+b <- which(BikeShare$casual %in% Outliers_cas$out)
+
+which(b %in% a)
+#Take out the outliers that overlap in total and casual bike rentals
+BikeShare <- BikeShare[-which(b %in% a),]
+
+ #BikeShare[,-c(2,3,4,5,6,7)]
+
+#Convert the temperatures back to standard temperatures
+BikeShare$temp <- round(BikeShare$temp * 39,digits = 0)
+BikeShare$atemp <- round(BikeShare$atemp * 50, digits = 0)
 
 #Create Weekday DF
 Weekdays <- c("Monday","Tuesday","Wednsday","Thursday","Friday","Saturday","Sunday")
@@ -34,14 +68,17 @@ colnames(BikeShare)[colnames(BikeShare) == "weekday"] <- "WeekdayNo"
 colnames(BikeShare)[colnames(BikeShare) == "hr"] <- "Hour"
 
 BikeShare <- merge(BikeShare, WeekdayDF, by = "WeekdayNo")
-rm(BikeShare1)
+#rm(BikeShare1)
 
 BikeShare$Weekdays <- as.factor(BikeShare$Weekdays)
 BikeShare$Weekdays <- ordered(BikeShare$Weekdays,levels = c("Monday","Tuesday","Wednsday","Thursday","Friday","Saturday","Sunday"))
-
+####Daily effects on use####
 Daily_Bikes <- BikeShare %>% 
                 group_by(Weekdays = BikeShare$Weekdays) %>% 
-                summarise(MeanDay = round(mean(cnt),digits = 0))
+                summarise(MeanDay = round(mean(cnt),digits = 0),
+                          Mean_cas = round(mean(casual),digits = 0),
+                          Mean_reg = round(mean(registered),digits = 0))
+Daily_Bikes <- Daily_Bikes %>% mutate(Casual_perc = round(Mean_cas/MeanDay,digits = 2))
 
 barplot(x = Daily_Bikes$Weekdays, y = Daily_Bikes$MeanDay)
 
@@ -50,29 +87,48 @@ ggplot(data = Daily_Bikes ,aes(x=Weekdays,y=MeanDay))+
   ggtitle("Weekly usage")+
   ylab("Average daily use")+
   coord_cartesian(ylim = c(150,200))
-
+####Hourly effects on use####
 Hourly_Bikes <- BikeShare %>%
                 group_by(Hour = BikeShare$Hour) %>%
-                summarise(MeanHour = round(mean(cnt),digits = 0))
+                summarise(MeanHour = round(mean(cnt),digits = 0),
+                          Mean_cas = round(mean(casual),digits = 0),
+                          Mean_reg = round(mean(registered),digits = 0))
 
+Hourly_Bikes <- Hourly_Bikes %>% mutate(Casual_perc = round(Mean_cas/MeanHour,digits = 2))
+####Seasonal effects on use####
+Season_Bikes <- BikeShare %>%
+                  group_by(Season = season) %>%
+                  summarise(Mean = round(mean(cnt),digits = 0),
+                            Mean_cas = round(mean(casual),digits = 0),
+                            Mean_reg = round(mean(registered),digits = 0))
+
+Season_Bikes <- Season_Bikes %>% mutate(Casual_perc = round(Mean_cas/Mean,digits = 2))
+  
 ggplot(data = Hourly_Bikes ,aes(x=Hour,y=MeanHour))+
   geom_bar(stat = "identity",fill = "orange")+
   ggtitle("Daily usage")+
   ylab("Average daily use")#+
   #coord_cartesian(ylim = c(150,200))
 
-###### TO DO #####################
-BikeShare1 <- BikeShare %>% mutate(Weather = ifelse(weathersit = 1, "good", ifelse(weathersit = 2, "cloudy",ifelse(weathersit = 3, "bad","very bad"))))
+Wettercode <- c(1,2,3,4)
+Wetter <- c("good","cloudy","bad","very bad")
+
+WetterDF <- data.frame(Wettercode,Wetter)
+
+BikeShare <- BikeShare %>% 
+                mutate(Weather = ifelse(weathersit == 1, "good", 
+                                  ifelse(weathersit == 2, "cloudy",
+                                  ifelse(weathersit == 3, "bad", "very bad"))))
 
 Weather_Bikes <- BikeShare %>% 
-                  group_by(Weather = BikeShare$weathersit) %>% 
+                  group_by(Weather = Weather) %>% 
                   summarise(Mean_util_total = round(mean(cnt),digits = 0),
                             Mean_util_casual = round(mean(casual),digits = 0),
                             Mean_util_reg = round(mean(registered),digits = 0))
 #Check if there the rental behaviour among casual and registered users changes when weather changes
-Weather_Bikes <- Weather_Bikes %>% mutate(cas_reg_relation = round(Mean_util_casual/Mean_util_reg,digits = 2))
+Weather_Bikes <- Weather_Bikes %>% mutate(Casual_perc = round(Mean_util_casual/Mean_util_total,digits = 2))
 
-ggplot(data = Weather_Bikes ,aes(x=Weather,y=Mean_utilisation))+
+ggplot(data = Weather_Bikes ,aes(x=Weather,y=Mean_util_total))+
   geom_bar(stat = "identity",fill = "orange")+
   ggtitle("Weather and usage")+
   ylab("Average use")#+
@@ -91,13 +147,100 @@ ggplot(data = Weather_Bikes ,aes(x=Weather,y=Mean_util_reg))+
 #coord_cartesian(ylim = c(150,200))
 
 
-CorData <- cor(BikeShare[c(4,6,7,10,11,14,17)])
+#CorData <- cor(BikeShare[c(1,4,6,7,10,11,14,17)])
+CorData <- cor(BikeShare[c(which(colnames(BikeShare) %in% c("season","mnth","Hour","weathersit","temp","atemp","windspeed","cnt","WeekdayNo")))])
+
 corrplot(CorData, method = "color")
+corrplot(CorData1, method = "color")
 corrplot(CorData, method = "number")
 
 #After checking for correlation, eliminate some redundant variables
 BikeShare[which(names(BikeShare)== "instant")] <- NULL
-BikeShare[which(names(BikeShare)== c("instant","windspeed","yr","season","holiday","Workingday","atemp","hum","windspeed"))] <- NULL
+BikeShare[which(names(BikeShare)== c("windspeed","yr","atemp","hum"))] <- NULL
 BikeShare[which(names(BikeShare)== c("holiday","workingday"))] <- NULL
-BikeShare[which(names(BikeShare)== "holiday")] <- NULL
 BikeShare[which(names(BikeShare)== "workingday")] <- NULL
+
+BikeShare1 <- BikeShare[,c("Date","Hour","temp","casual","registered","cnt","Weekdays","WeekdayNo")]
+#Taking out same outliers as for the BikeShare DF
+BikeShare1 <- BikeShare1[-which(b %in% a),]
+
+# What is the average usage of bikes per day on a Monday compared to the overall average?
+mean(BikeShare1$cnt)
+mean(BikeShare1$cnt[which(BikeShare1$Weekdays == "Monday")])
+which(BikeShare1$Weekdays == "Monday")
+
+#### Start building the regression model ####
+#kNN Model__________________________________________________________________________
+set.seed(123)
+Train_Model<-createDataPartition(y=BikeShare1$cnt,p=.75,list = FALSE)
+#str(Train_Model)
+#TRAIN_Model
+TrainingSet<-BikeShare1[Train_Model,]
+TestingSet<-BikeShare1[-Train_Model,]
+
+#nrow(training)
+#nrow(testing)
+kNNControl<-trainControl(method = "repeatedcv",#This_is_CROSS_VALIDATION
+                         number = 5,
+                         repeats = 5,
+                         verboseIter = TRUE)#,#verbose =TRUE
+#classProbs = TRUE)
+#summaryFunction = twoClassSummary)
+#RUN_MODEL
+set.seed(111)
+knnFit<-train(cnt ~ Hour+temp+atemp+hum+WeekdayNo+windspeed,#alternative_being:_(brand~.,)
+              data = TrainingSet,
+              method = "knn",
+              trControl=kNNControl,
+              tuneLength = 20,
+              preProc = c("center", "scale")) #only center and scale integer variables for regression???
+knnFit
+#Predict
+kNN_Predict<-predict(knnFit,newdata = TestingSet)
+#str(kNN_Predict)
+#plot(knnFit)
+kNN_Predict
+kNN_Probs<-predict(knnFit,newdata = TestingSet,type = "prob")
+head(kNN_Probs)
+#Use a Confusion Matrix only for classification problems!!!
+#confusionMatrix(data = kNN_Predict,TestingSet$cnt)
+
+#the postResample function is being used to estimate the Root mean squared error
+postResample(kNN_Predict,TestingSet$cnt)
+#Apply the model to Validation data
+kNN_Predict_Validation<-predict(knnFit,newdata = Wifi_Vald)
+kNN_Predict_Validation
+postResample(kNN_Predict_Validation,Wifi_Vald$UniquePosit)
+
+
+#SVM Model__________________________________________________________________________
+SVMControl<-trainControl(method = "repeatedcv",#This_is_CROSS_VALIDATION
+                   number = 10,
+                   repeats = 10,
+                   verboseIter = TRUE)#,#verbose =TRUE
+set.seed(111)
+SVM_Fit<-train(registered~Hour+temp+hum,#alternative_being:_(Volume~.,)
+               data = TrainingSet,
+               method = "svmLinear",
+               trControl=SVMControl,
+               tuneLength = 15,
+               preProc = c("center", "scale"))
+SVM_Fit
+#Predict
+SVM_Predict<-predict(SVM_Fit,newdata = TestingSet)
+
+postResample(SVM_Predict,TestingSet$registered)
+
+#Random Forest Model__________________________________________________________________________
+
+RF_Fit<-train(registered~Hour+temp+hum,#alternative_being:_(Volume~.,)
+               data = TrainingSet,
+               method = "rf",
+               trControl=SVMControl,
+               tuneLength = 10,
+               preProc = c("center", "scale"))
+RF_Fit
+#Predict
+RF_Predict<-predict(RF_Fit,newdata = TestingSet)
+
+postResample(RF_Predict,TestingSet$registered)
